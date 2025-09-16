9acf9db2693ab06697ebf1ab22fa63cb818f49f4
Adam Silver <adambsilver@gmail.com>
2025-09-16 14:29:59 +0100

    Add activity placeholder

---
+++ app/routes/case--activity.js
@@ -0,0 +1,24 @@
+const _ = require('lodash')
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+module.exports = router => {
+  router.get("/cases/:id/activity", async (req, res) => {
+    const _case = await prisma.case.findUnique({
+      where: { id: parseInt(req.params.id) },
+      include: { 
+        user: true, 
+        witnesses: { include: { statements: true } }, 
+        lawyers: true, 
+        defendants: true, 
+        hearing: true, 
+        location: true, 
+        tasks: true, 
+        dga: true 
+      }
+    })
+
+    res.render("cases/activity/index", { _case })
+  })
+
+}
