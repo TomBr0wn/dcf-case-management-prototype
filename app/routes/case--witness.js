2857af6e7fcae9cb93adec484a50110fead1de49
Adam Silver <adambsilver@gmail.com>
2025-10-13 16:20:23 +0100

    Show witness details page

--- app/routes/case--witness.js
+++ app/routes/case--witness.js
@@ -3,7 +3,7 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 module.exports = router => {
-  
+
   router.get("/cases/:caseId/witnesses/:witnessId", async (req, res) => {
     const _case = await prisma.case.findUnique({
       where: { id: parseInt(req.params.caseId) },
@@ -11,10 +11,62 @@ module.exports = router => {
     })
 
     const witness = await prisma.witness.findUnique({
-      where: { id: parseInt(req.params.witnessId )}
+      where: { id: parseInt(req.params.witnessId) },
+      include: { statements: true }
     })
 
-    res.render("cases/witnesses/show", { 
+    res.render("cases/witnesses/show", {
+      _case,
+      witness
+    })
+  })
+
+  router.get("/cases/:caseId/witnesses/:witnessId/statements", async (req, res) => {
+    const _case = await prisma.case.findUnique({
+      where: { id: parseInt(req.params.caseId) },
+      include: { defendants: true, witnesses: true },
+    })
+
+    const witness = await prisma.witness.findUnique({
+      where: { id: parseInt(req.params.witnessId) },
+      include: { statements: true }
+    })
+
+    res.render("cases/witnesses/statements", {
+      _case,
+      witness
+    })
+  })
+
+  router.get("/cases/:caseId/witnesses/:witnessId/availability", async (req, res) => {
+    const _case = await prisma.case.findUnique({
+      where: { id: parseInt(req.params.caseId) },
+      include: { defendants: true, witnesses: true },
+    })
+
+    const witness = await prisma.witness.findUnique({
+      where: { id: parseInt(req.params.witnessId) },
+      include: { statements: true }
+    })
+
+    res.render("cases/witnesses/availability", {
+      _case,
+      witness
+    })
+  })
+
+  router.get("/cases/:caseId/witnesses/:witnessId/special-measures", async (req, res) => {
+    const _case = await prisma.case.findUnique({
+      where: { id: parseInt(req.params.caseId) },
+      include: { defendants: true, witnesses: true },
+    })
+
+    const witness = await prisma.witness.findUnique({
+      where: { id: parseInt(req.params.witnessId) },
+      include: { statements: true }
+    })
+
+    res.render("cases/witnesses/special-measures", {
       _case,
       witness
     })
